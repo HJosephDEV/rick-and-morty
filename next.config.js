36111f45412dca60cc495927744bae79a28346e6
Joseph Haase <joseph.lima@codenapp.com>
2023-06-28 15:28:15 -0300

    Adds new settings of eslint and prettier

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
   }
 };
-
-module.exports = nextConfig;
