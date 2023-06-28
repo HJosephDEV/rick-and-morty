491a412522bfc10162aec9b9897201e937e91601
Joseph Haase <joseph.lima@codenapp.com>
2023-06-28 15:31:39 -0300

    Creates footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer id="footer-layout">
+      <p>
+        Desenvolvido por <b>Joseph Haase</b> ☕
+      </p>
+    </footer>
+  );
+}
