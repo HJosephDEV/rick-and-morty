6e041b6e80c5374a01eb6ff5be81e1b99dff31a6
Joseph Haase <joseph.haase.dev@gmail.com>
2023-07-16 19:06:14 -0300

    Fixes loading of images of the cards

--- src/app/components/Card/components/CardImage/index.tsx
+++ src/app/components/Card/components/CardImage/index.tsx
@@ -6,6 +6,13 @@ export default function CardImage({ imageSrc }: CardImage) {
   return (
     <div className="card-image-container">
       <img
+        className="image__wrapper"
+        src="assets/images/white.png"
+        alt="background"
+      />
+
+      <img
+        className="card__image"
         src={imageSrc}
         alt="image of character"
       />
